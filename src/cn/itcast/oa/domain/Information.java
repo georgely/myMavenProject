a9a4e2ac3b740dea9eb08520f2389fa859866c4e
crl <crl_228@163.com>
2015-09-02 00:08:11 +0800

    1 增加struts2支持 2 暂时去除输入验证

--- src/cn/itcast/oa/domain/Information.java
+++ src/cn/itcast/oa/domain/Information.java
@@ -11,67 +11,92 @@ public class Information {
 	private Long zipcode;
 	private String country;
 	private String message;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getAddress1() {
 		return address1;
 	}
+
 	public void setAddress1(String address1) {
 		this.address1 = address1;
 	}
+
 	public String getAddress2() {
 		return address2;
 	}
+
 	public void setAddress2(String address2) {
 		this.address2 = address2;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public Long getZipcode() {
 		return zipcode;
 	}
+
 	public void setZipcode(Long zipcode) {
 		this.zipcode = zipcode;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
 
+	@Override
+	public String toString() {
+		return "Information [id=" + id + ", name=" + name + ", email=" + email
+				+ ", address1=" + address1 + ", address2=" + address2
+				+ ", city=" + city + ", state=" + state + ", zipcode=" + zipcode
+				+ ", country=" + country + ", message=" + message + "]";
+	}
 }
